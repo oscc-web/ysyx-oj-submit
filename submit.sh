314293ed64d60f009cd2dba7d5ba7609fc8dbf1e
myyerrol <myyerrol@126.com>
2023-04-05 10:37:20 +0800

    style: add cmd args

--- submit.sh
+++ submit.sh
@@ -1,11 +1,18 @@
 
 TOKEN="xxx"
 PROBLEM_ID="xxx"
-FILE=/tmp/upload.tar.bz2
+FILE="/tmp/upload.tar.bz2"
+
+# 不要编辑以下内容
+URL="39.101.140.145"
+if [ "$1" = "-dev" ]; then
+    URL="127.0.0.1"
+fi
+echo "URL: $URL"
 
 tar -cf "$FILE" --exclude=".git" --exclude="submit.sh" "./"
 
 curl -F "token=$TOKEN" \
      -F "problemId=$PROBLEM_ID" \
      -F "upload=@$FILE" \
-    http://127.0.0.1:5050/api/uploadFileByScript
+    http://$URL:10140/api/uploadFileByScript
